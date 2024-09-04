afd70e9dd98662766d83eb333497b117d33ed567
mathisdlg <delage.mathis.1@gmail.com>
2024-09-04 11:29:34 +0200

    Add some user settings

--- user/vscodium/vscodium.nix
+++ user/vscodium/vscodium.nix
@@ -1,29 +1,63 @@
 { config, pkgs, ... }: {
-	
-	programs.vscode = {
-		enable = true;
-		package = pkgs.vscodium;
-		extensions = with pkgs.vscode-extensions; [
-			# Copilot
-			github.copilot
+	imports = [];
 
-			# Theme
-			# One dark pro
-			zhuangtongfa.material-theme
-			# Material icon
-			pkief.material-icon-theme
-
-			# Bookmark
-			alefragnani.bookmarks
-
-			# Code Snap
-			adpyke.codesnap
-
-			# Programmation
-			# Nix
-			bbenoist.nix
-			# Python pack
-			ms-python.python
+	config = {
+		home.packages = with pkgs; [
+			jetbrains-mono
 		];
+
+		programs.vscode = {
+			enable = true;
+
+			package = pkgs.vscodium;
+			
+			extensions = with pkgs.vscode-extensions; [
+				# Copilot
+				github.copilot
+
+				# Theme
+				# One dark pro
+				zhuangtongfa.material-theme
+				# Material icon
+				pkief.material-icon-theme
+
+				# Bookmark
+				alefragnani.bookmarks
+
+				# Code Snap
+				adpyke.codesnap
+
+				# Programmation
+				# Nix
+				bbenoist.nix
+				# Python pack
+				ms-python.python
+			];
+
+			userSettings = {
+				"files.autoSave"="afterDelay";
+
+				"workbench.colorTheme"="One Dark Pro";
+				"workbench.iconTheme"="material-icon-theme";
+
+				"git.confirmSync"=false;
+				"git.autofetch"=true;
+
+				"editor.fontFamily"="'JetBrains Mono'";
+				"editor.fontWeight"="normal";
+				"editor.fontLigatures"=true;
+				"editor.smoothScrolling"=true;
+				"editor.rulers"=[
+					{
+						"column"=120;
+						"color"="#aae5a4";
+					}
+				];
+				"editor.tabSize"=4;
+				"workbench.colorCustomizations"={
+					"editorRuler.foreground"="#00000000";
+				};
+			};
+		};
 	};
 }
