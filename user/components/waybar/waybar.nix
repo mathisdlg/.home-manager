f2c62e1fe9a78c45de87d48664b17254694e724e
mathisdlg <delage.mathis.1@gmail.com>
2024-09-04 11:29:15 +0200

    Add package jetbrains mono to have all the needed icons

--- user/components/waybar/waybar.nix
+++ user/components/waybar/waybar.nix
@@ -1,7 +1,11 @@
-{ config, ... }: {
+{ config, pkgs, ... }: {
 	imports = [];
 
 	config = {
+		home.packages = with pkgs; [
+			jetbrains-mono
+		];
+
 		home.file = {
 			".config/waybar/config".source = ./config;
             ".config/waybar/style.css".source = ./style.css;
