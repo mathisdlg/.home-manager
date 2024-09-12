b618d28d6358fda5891e756e9180954f5b1d2e62
mathisdlg <delage.mathis.1@gmail.com>
2024-09-12 12:47:09 +0200

    Change to stable version of nix and home manager

--- flake.nix
+++ flake.nix
@@ -2,9 +2,9 @@
 	description = "Home manager flake";
 
 	inputs = {
-		nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+		nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
 		home-manager = {
-			url = "github:nix-community/home-manager/master";
+			url = "github:nix-community/home-manager/release-24.05";
 			inputs.nixpkgs.follows = "nixpkgs";
 		};
 	};
