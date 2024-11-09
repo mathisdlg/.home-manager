8e8c3d10fc714015cdd2315764cd9b0a4d4bd2b2
mathisdlg <delage.mathis.1@gmail.com>
2024-11-09 22:47:15 +0100

    Add a good config for wlogout

--- user/components/wlogout/wlogout.nix
+++ user/components/wlogout/wlogout.nix
@@ -5,7 +5,14 @@
 		home = {
 			packages = with pkgs; [
 				wlogout
+				fira-code
 			];
+
+			file = {
+				".config/wlogout/layout".source = ./config/layout;
+				".config/wlogout/icons".source = ./config/icons;
+				".config/wlogout/style.css".source = ./config/style.css;
+			};
 		};
 	};
 }
