6247b9d7508d58e1f3726f50235a902c89ae61fa
mathisdlg <delage.mathis.1@gmail.com>
2024-11-13 15:05:10 +0100

    Change update command (rebuild boot instead of rebuild switch)

--- user/scripts/update.sh
+++ user/scripts/update.sh
@@ -2,7 +2,7 @@ printf "Flake update\n"
 nix flake update /home/mathisdlg/.home-manager
 
 printf "\nNixOs rebuild switch\n"
-sudo nixos-rebuild switch --flake /home/mathisdlg/.home-manager
+sudo nixos-rebuild boot --flake /home/mathisdlg/.home-manager
 
 printf "\nHome Manager rebuild switch\n"
 home-manager switch --flake /home/mathisdlg/.home-manager
