435d58ea915f875836792d245d626cee5d99d6ea
mathisdlg <delage.mathis.1@gmail.com>
2024-12-29 19:13:29 +0100

    Add enable options in baobab

--- user/baobab/baobab.nix
+++ user/baobab/baobab.nix
@@ -1,7 +1,10 @@
-{ config, pkgs, ... }: {
-	imports = [];
+{ config, pkgs, lib, ... }:
+with lib; let cfg = config.services.baobab; in {
+	options.services = {
+		baobab.enable = mkEnableOption "Enable baobab disk usage analyzer.";
+	};
 
-	config = {
+	config = mkIf cfg.enable {
 		home.packages = with pkgs; [
 			baobab
 		];
