720fad69fe57e1f72a2a6e54db40903fe9cf758f
mathisdlg <delage.mathis.1@gmail.com>
2024-12-30 01:09:04 +0100

    Change puzzles.nix in module with enable option

--- user/games/puzzles/puzzles.nix
+++ user/games/puzzles/puzzles.nix
@@ -1,7 +1,8 @@
-{ config, pkgs, ... }: {
-	imports = [];
+{ config, pkgs, lib, ... }:
+with lib; let cfg = config.services.puzzles; in {
+	options.services.puzzles.enable = mkEnableOption "Enable puzzles games (https://www.chiark.greenend.org.uk/~sgtatham/puzzles/).";
 
-	config = {
+	config = mkIf cfg.enable {
 		home.packages = with pkgs; [
 			sgt-puzzles
 		];
