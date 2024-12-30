002cec1f3f02a4268755587f20373a15b4ff74f4
mathisdlg <delage.mathis.1@gmail.com>
2024-12-30 01:07:09 +0100

    Move notifications in component and change dunst.nix in module with enable option

--- user/components/notifications/dunst/dunst.nix
+++ user/components/notifications/dunst/dunst.nix
@@ -1,5 +1,8 @@
-{ config, lib, pkgs, ... }: {
-	config = {
+{ config, pkgs, lib, ... }:
+with lib; let cfg = config.services.dunst; in {
+	options.services.dunst.enable = mkEnableOption "Enable dunst (notifications manager).";
+
+	config = mkIf cfg.enable {
 		home.packages = with pkgs; [
 			libnotify
 		];
