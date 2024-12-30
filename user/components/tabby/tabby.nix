0077f1191eaf99b91aed10038dbd21c0d5bdd0d2
mathisdlg <delage.mathis.1@gmail.com>
2024-12-30 01:07:15 +0100

    Change tabby.nix in module with enable option

--- user/components/tabby/tabby.nix
+++ user/components/tabby/tabby.nix
@@ -1,7 +1,8 @@
-{ config, pkgs, ... }: {
-	imports = [];
+{ config, pkgs, lib, ... }:
+with lib; let cfg = config.services.tabby; in {
+	options.services.tabby.enable = mkEnableOption "Enable TabbyML (Self-hosted AI coding assistant).";
 
-	config = {
+	config = mkIf cfg.enable {
 		home.packages = with pkgs; [
 			tabby
 		];
