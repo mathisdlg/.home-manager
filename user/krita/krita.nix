8739ff928fef07bb36bfbb894dbf534163cba0b6
mathisdlg <delage.mathis.1@gmail.com>
2024-12-30 01:09:27 +0100

    Change krita.nix in module with enable option

--- user/krita/krita.nix
+++ user/krita/krita.nix
@@ -1,7 +1,8 @@
-{ config, pkgs, ... }: {
-	imports = [];
+{ config, pkgs, lib, ... }:
+with lib; let cfg = config.services.krita; in {
+	options.services.krita.enable = mkEnableOption "Enable krita.";
 
-	config = {
+	config = mkIf cfg.enable {
 		home.packages = with pkgs; [
 			krita
 		];
