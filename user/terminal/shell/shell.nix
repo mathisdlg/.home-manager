7d029f5d786d675e657dbbfe9a692cf399a8d8e1
mathisdlg <delage.mathis.1@gmail.com>
2024-12-30 01:09:51 +0100

    Change shell.nix in module with enable option

--- user/terminal/shell/shell.nix
+++ user/terminal/shell/shell.nix
@@ -1,14 +1,18 @@
-{ config, pkgs, ... }:
-
-let
+{ config, pkgs, lib, ... }:
+with lib; let
 	myAliases = {
 		update="bash /home/mathisdlg/.update.sh";
 		fupdate="nix flake update";
 		tupdate="nvim ~/.config/nix/nix.conf";
 	};
+	cfg = config.services.bash;
 in {
-	programs.bash = {
-		enable = true;
-		shellAliases = myAliases;
+	options.services.osu.enable = mkEnableOption "Enable bash.";
+
+	config = mkIf cfg.enable {
+		programs.bash = {
+			enable = true;
+			shellAliases = myAliases;
+		};
 	};
 }
