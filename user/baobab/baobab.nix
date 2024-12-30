904175f57ef18b9817d73763d577aa8ef5c42397
mathisdlg <delage.mathis.1@gmail.com>
2024-12-30 01:03:06 +0100

    Optimize options in baobab

--- user/baobab/baobab.nix
+++ user/baobab/baobab.nix
@@ -1,8 +1,6 @@
 { config, pkgs, lib, ... }:
 with lib; let cfg = config.services.baobab; in {
-	options.services = {
-		baobab.enable = mkEnableOption "Enable baobab disk usage analyzer.";
-	};
+	options.services.baobab.enable = mkEnableOption "Enable baobab disk usage analyzer.";
 
 	config = mkIf cfg.enable {
 		home.packages = with pkgs; [
