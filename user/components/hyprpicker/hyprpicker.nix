d08388fd38b7393022f42ff1442c78a39ea9461e
mathisdlg <delage.mathis.1@gmail.com>
2024-12-30 01:06:35 +0100

    Change hyprpicker.nix in module with enable option

--- user/components/hyprpicker/hyprpicker.nix
+++ user/components/hyprpicker/hyprpicker.nix
@@ -1,7 +1,8 @@
-{ config, pkgs, ... }: {
-	imports = [];
+{ config, pkgs, lib, ... }:
+with lib; let cfg = config.services.hyprpicker; in {
+	options.services.hyprpicker.enable = mkEnableOption "Enable hyprland color picker.";
 
-	config = {
+	config = mkIf cfg.enable {
 		home.packages = with pkgs; [
 			hyprpicker
 			wl-clipboard
