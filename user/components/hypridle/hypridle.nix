f883b0211c7972f898b2221c1f735e764be70591
mathisdlg <delage.mathis.1@gmail.com>
2024-12-31 12:29:42 +0100

    update hypridle option name

--- user/components/hypridle/hypridle.nix
+++ user/components/hypridle/hypridle.nix
@@ -1,16 +1,14 @@
 { config, pkgs, lib, ... }:
-with lib; let cfg = config.services.hypridle; in {
-	options.services.hypridle.enable = mkEnableOption "Enable hyprland idle manager.";
+with lib; let cfg = config.services.hypridle-custom; in {
+	options.services.hypridle-custom.enable = mkEnableOption "Enable hyprland idle manager.";
 
 	config = mkIf cfg.enable {
 		home = {
-			packages = with pkgs; [
-				hypridle
-			];
-
 			file = {
 				".config/hypr/hypridle.conf".source = ./hypridle.conf;
 			};
 		};
+
+		services.hypridle.enable = true;
 	};
 }
