b375cc169826c0e6e191c76ae988b1ff8475730e
mathisdlg <delage.mathis.1@gmail.com>
2024-12-31 12:37:30 +0100

    fix dunst-custom option name

--- user/components/notifications/dunst/dunst.nix
+++ user/components/notifications/dunst/dunst.nix
@@ -1,6 +1,6 @@
 { config, pkgs, lib, ... }:
 with lib; let cfg = config.services.dunst-custom; in {
-	options.services.dunst-.enable = mkEnableOption "Enable dunst (notifications manager).";
+	options.services.dunst-custom.enable = mkEnableOption "Enable dunst (notifications manager).";
 
 	config = mkIf cfg.enable {
 		home.packages = with pkgs; [
