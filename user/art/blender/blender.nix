90d823f0071755d5b222d31ca4837eda6d8efc41
mathisdlg <delage.mathis.1@gmail.com>
2024-12-31 15:07:17 +0100

    rename blender and krite option name

--- user/art/blender/blender.nix
+++ user/art/blender/blender.nix
@@ -1,6 +1,6 @@
 { config, pkgs, lib, ... }:
-with lib; let cfg = config.services.blender; in {
-	options.services.blender.enable = mkEnableOption "Enable blender.";
+with lib; let cfg = config.services.art.blender; in {
+	options.services.art.blender.enable = mkEnableOption "Enable blender.";
 
 	config = mkIf cfg.enable {
 		home.packages = with pkgs; [
--- user/art/krita/krita.nix
+++ user/art/krita/krita.nix
@@ -1,6 +1,6 @@
 { config, pkgs, lib, ... }:
-with lib; let cfg = config.services.krita; in {
-	options.services.krita.enable = mkEnableOption "Enable krita.";
+with lib; let cfg = config.services.art.krita; in {
+	options.services.art.krita.enable = mkEnableOption "Enable krita.";
 
 	config = mkIf cfg.enable {
 		home.packages = with pkgs; [
