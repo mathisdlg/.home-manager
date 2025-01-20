faad006ca8a7f387e52382ff45fd4adbd996c33b
mathisdlg <delage.mathis.1@gmail.com>
2025-01-20 16:13:51 +0100

    Add missing ; and s in importS

--- user/screenshare/screenshare.nix
+++ user/screenshare/screenshare.nix
@@ -2,17 +2,17 @@
 with lib; let cfg = config.services.screenshare.screenshare; in {
 	options.services.screenshare.screenshare.enable = mkEnableOption "Enable screensharing app.";
 
-	import = [
+	imports = [
 		./grimblast/grimblast.nix
 		./screenrec/screenrec.nix
 		./slurp/slurp.nix
 	];
 
-	services = mkIf cfg.enable {
+	config.services = mkIf cfg.enable {
 		screenshare = {
 			grimblast.enable = true;
 			screenrec.enable = true;
 			slurp.enable = true;
 		};
-	}
+	};
 }
