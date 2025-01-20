e3a777c5c57c25a645fffb77a5a905ae461aed9b
mathisdlg <delage.mathis.1@gmail.com>
2025-01-20 15:28:50 +0100

    Add screenshot, screenrec and slurp in screenshare

---
+++ user/screenshare/grimblast/grimblast.nix
@@ -0,0 +1,10 @@
+{ config, pkgs, lib, ... }:
+with lib; let cfg = config.services.screenshare.grimblast; in {
+	options.services.screenshare.grimblast.enable = mkEnableOption "Enable grimblast.";
+
+	config = mkIf cfg.enable {
+		home.packages = with pkgs; [
+			grimblast
+		];
+	};
+}
---
+++ user/screenshare/slurp/slurp.nix
@@ -0,0 +1,10 @@
+{ config, pkgs, lib, ... }:
+with lib; let cfg = config.services.screenshare.slurp; in {
+	options.services.screenshare.slurp.enable = mkEnableOption "Enable slurp.";
+
+	config = mkIf cfg.enable {
+		home.packages = with pkgs; [
+			slurp
+		];
+	};
+}
