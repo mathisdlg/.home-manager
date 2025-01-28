ce697013c8f9fc9fd1b1c10cb180e1837239750b
mathisdlg <delage.mathis.1@gmail.com>
2025-01-28 15:44:58 +0100

    Fix typo in mimssion center

--- user/system-monitor/mission-center/mission-center.nix
+++ user/system-monitor/mission-center/mission-center.nix
@@ -1,6 +1,6 @@
 { config, pkgs, lib, ... }:
 with lib; let cfg = config.services.system-monitor.mission-center; in {
-	options.services.system-monitor.gnome-system-monitor.enable = mkEnableOption "Enable mission center (a systetm monitor).";
+	options.services.system-monitor.mission-center.enable = mkEnableOption "Enable mission center (a systetm monitor).";
 
 	config = mkIf cfg.enable {
 		home.packages = with pkgs; [
