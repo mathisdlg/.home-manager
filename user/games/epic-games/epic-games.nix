268c7502e4dba34761944cd11d6061429852f514
mathisdlg <delage.mathis.1@gmail.com>
2025-01-30 15:10:39 +0100

    Fix typo

--- user/games/epic-games/epic-games.nix
+++ user/games/epic-games/epic-games.nix
@@ -1,6 +1,6 @@
 { config, pkgs, lib, ... }:
 with lib; let cfg = config.services.games.epic-games; in {
-	options.services.games.epic-games.enable = mkEnableOption "Enable gnome epic games store.";
+	options.services.games.epic-games.enable = mkEnableOption "Enable epic games store.";
 
 	config = mkIf cfg.enable {
 		home.packages = with pkgs; [
