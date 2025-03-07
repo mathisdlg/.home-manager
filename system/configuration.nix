cbd23d7a8e3f6b50d7bc6e444456b09b218fd580
mathisdlg <delage.mathis.1@gmail.com>
2025-03-07 15:10:59 +0100

    OPen port 6060 to test an home made application

--- system/configuration.nix
+++ system/configuration.nix
@@ -264,7 +264,7 @@
 	# services.openssh.enable = true;
 
 	# Open ports in the firewall.
-	# networking.firewall.allowedTCPPorts = [ ... ];
+	networking.firewall.allowedTCPPorts = [ 6060 ];
 	# networking.firewall.allowedUDPPorts = [ ... ];
 	# Or disable the firewall altogether.
 	# networking.firewall.enable = false;
