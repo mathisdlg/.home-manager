28c0c87d09344438ee97a1458cb6ba756eb9d56c
mathisdlg <delage.mathis.1@gmail.com>
2025-05-26 08:05:45 +0200

    Change to 25.05 channel

--- flake.nix
+++ flake.nix
@@ -2,9 +2,9 @@
 	description = "Home manager flake";
 
 	inputs = {
-		nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+		nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
 		home-manager = {
-			url = "github:nix-community/home-manager/release-24.11";
+			url = "github:nix-community/home-manager/release-25.05";
 			inputs.nixpkgs.follows = "nixpkgs";
 		};
 	};
