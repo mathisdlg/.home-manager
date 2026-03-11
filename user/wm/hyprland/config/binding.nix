6f1a55257a119c86834707cd4c9851842d4181b8
mathisdlg <delage.mathis.1@gmail.com>
2026-03-11 18:47:04 +0100

    Replace socat commands with playerctl for media control in Hyprland configuration

--- user/wm/hyprland/config/binding.nix
+++ user/wm/hyprland/config/binding.nix
@@ -111,10 +111,10 @@
           "$mainMod, D, exec, $discord"
 
           # Music controller for mpv
-          ", XF86AudioNext, exec, echo 'playlist-next' | socat - /tmp/mpvsocket"
-          ", XF86AudioPrev, exec, echo 'playlist-prev' | socat - /tmp/mpvsocket"
-          ", XF86AudioPlay, exec, echo 'cycle pause' | socat - /tmp/mpvsocket"
-          ", XF86AudioStop, exec, echo 'stop' | socat - /tmp/mpvsocket"
+          ", XF86AudioNext, exec, playerctl next"
+          ", XF86AudioPrev, exec, playerctl previous"
+          ", XF86AudioPlay, exec, playerctl play-pause"
+          ", XF86AudioStop, exec, playerctl stop"
 
           # Screenshot
           ", Print, exec, grimblast copysave active"
